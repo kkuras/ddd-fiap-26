1a668c0fe228345cb45d4e7766902ec07d68101f
kkura <flkuratomijr@gmail.com>
2026-08-15 17:57:56 -0300

    terminei? a conexao

--- ddd-agenda/src/br/com/fiap/agenda/dao/ContatoDAO.java
+++ ddd-agenda/src/br/com/fiap/agenda/dao/ContatoDAO.java
@@ -5,6 +5,7 @@ import br.com.fiap.agenda.models.Contato;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 // classe responsavel pelo crud de contato
 public class ContatoDAO {
@@ -16,6 +17,20 @@ public class ContatoDAO {
         try {
             String sql = "insert into tbl_contato (ID_CONTATO, NOME_CONTATO, CELULAR_CONTATO, EMAIL_CONTATO, INSTAGRAM, TIPO)" +
                     "values(?, ?, ?, ?, ?, ?)";
+
+            comandoSQL = conexao.prepareStatement(sql);
+            comandoSQL.setInt(1, contato.getId());
+            comandoSQL.setString(2, contato.getNome());
+            comandoSQL.setString(3, contato.getCelular());
+            comandoSQL.setString(4, contato.getEmail());
+            comandoSQL.setString(5, contato.getInstagram());
+            comandoSQL.setString(6, contato.getTipo());
+
+            comandoSQL.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
+
     }
 }
--- ddd-agenda/src/br/com/fiap/agenda/tests/TesteConexao.java
+++ ddd-agenda/src/br/com/fiap/agenda/tests/TesteConexao.java
@@ -3,7 +3,7 @@ package br.com.fiap.agenda.tests;
 import br.com.fiap.agenda.dao.ConnectionFactory;
 
 public class TesteConexao {
-    static void main() {
+    public static void main(String[] args) {
         System.out.println("eu como coco dos bancos de dados");
         if (ConnectionFactory.obterconexao() == null) {
             System.out.println("deu erro");
