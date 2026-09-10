a6d8c994fd0f0faa35f0bbe53c55dcc86f3ebda9
kkura <flkuratomijr@gmail.com>
2026-09-10 08:06:06 -0300

    agendaweb

---
+++ agendaweb/src/test/java/fiap/com/br/agendaweb/AgendawebApplicationTests.java
@@ -0,0 +1,13 @@
+package fiap.com.br.agendaweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendawebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
