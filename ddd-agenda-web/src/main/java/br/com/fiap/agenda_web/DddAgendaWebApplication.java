bcdd4366e99bc3aec65143a5e64ddb7600361f69
kkura <flkuratomijr@gmail.com>
2026-09-10 09:01:52 -0300

    unversioned

---
+++ ddd-agenda-web/src/main/java/br/com/fiap/agenda_web/DddAgendaWebApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.agenda_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddAgendaWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DddAgendaWebApplication.class, args);
+	}
+
+}
---
+++ ddd-agenda-web/src/test/java/br/com/fiap/agenda_web/DddAgendaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.agenda_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DddAgendaWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
