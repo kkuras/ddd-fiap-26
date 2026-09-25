370a519c726fb43a110da9c0fab457f01b961e4b
kkura <flkuratomijr@gmail.com>
2026-09-24 23:53:04 -0300

    listar controller

--- cineFiap/src/main/java/br/com/fiap/cineFiap/resource/FilmeController.java
+++ cineFiap/src/main/java/br/com/fiap/cineFiap/resource/FilmeController.java
@@ -40,6 +40,12 @@ public class FilmeController {
 
     }
 
+    @GetMapping
+    public ResponseEntity <List<Filme>> listar {
+        return ResponseEntity.ok(service.listartodos());
+
+    }
+
 
 
 
